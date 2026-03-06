f1412cfd127e5a227a7051f27759a7f69e136f38
EricJK_Briones <129333560+EricJKBriones@users.noreply.github.com>
2026-03-06 12:45:33 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("sql312.infinityfree.com", "if0_41308052", "WIqpwAySXz", "if0_41308052_musuem_db");
+$conn = mysqli_connect("localhost", "root", "", "museum_db");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
